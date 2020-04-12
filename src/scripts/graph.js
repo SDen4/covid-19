eb9d51a40e469157b29a96d1f881c331d4082d95
SDen4 <asw325@yandex.ru>
2020-04-12 16:54:10 +0500

    dynamic data is working!

--- src/scripts/graph.js
+++ src/scripts/graph.js
@@ -2,7 +2,7 @@ import ill from "../json/ill.json";
 import death from "../json/dead.json";
 import recover from "../json/recover.json";
 
-let textTitle = 'Динамика распространения короновируса в России';
+let textTitle = 'Распространение короновируса в России';
 
 
 //arrays of data: illness, deaths and recovers
@@ -17,8 +17,6 @@ for (let item in death) {
 for (let item in recover) {
     recovers.push(recover[item]);
 };
-
-
 
 
 //detailes data
@@ -42,104 +40,97 @@ for(let j = 0; j < recovers.length-1; j++ ) {
     dynamicRecovers.push(delta);
 };
 
-
-
-
-
-
-
 // Set up the chart
-var chart = new Highcharts.Chart({
-    chart: {
-        renderTo: 'container',
-        type: 'column',
-        zoomType: "x",
-        options3d: {
-            enabled: true,
-            alpha: 15,
-            beta: 15,
-            depth: 50,
-            viewDistance: 25
-        }
-    },
-    credits: {
-        text: "SDen4",
-        href: "https://github.com/SDen4",
-        style: {
-            fontSize: "16px",
-            color: "#A53E3E"
-        }
-
-    },
-    colors: ['#D2691E', '#4B0082', '#008000'],
-    title: {
-        text: textTitle
-    },
-    yAxis: {
+function chartFunction(dates, illness, deaths, recovers, textTitle) {
+    var chart = new Highcharts.Chart({
+        chart: {
+            renderTo: 'container',
+            type: 'column',
+            zoomType: "x",
+            options3d: {
+                enabled: true,
+                alpha: 15,
+                beta: 15,
+                depth: 50,
+                viewDistance: 25
+            }
+        },
+        credits: {
+            text: "SDen4",
+            href: "https://github.com/SDen4",
+            style: {
+                fontSize: "16px",
+                color: "#A53E3E"
+            }
+        },
+        colors: ['#D2691E', '#4B0082', '#008000'],
         title: {
-            text: "Кол-во человек"
-        }
-    },
-    xAxis: {
-        categories: dates
-    },
-    subtitle: {
-        text: 'Источники инофрмации: WHO, Яндекс'
-    },
-    plotOptions: {
-        column: {
-            depth: 25
-        }
-    },
-    series: [{
-        name: "Заболело",
-        data: illness
-    },{
-        name: "Умерло",
-        data: deaths
-    },{
-        name: "Выздоровело",
-        data: recovers
-    }],
-    
-});
+            text: textTitle
+        },
+        yAxis: {
+            title: {
+                text: "Кол-во человек"
+            }
+        },
+        xAxis: {
+            categories: dates
+        },
+        subtitle: {
+            text: 'Источники инофрмации: WHO, Яндекс'
+        },
+        plotOptions: {
+            column: {
+                depth: 25
+            }
+        },
+        series: [{
+            name: "Заболело",
+            data: illness
+        },{
+            name: "Умерло",
+            data: deaths
+        },{
+            name: "Выздоровело",
+            data: recovers
+        }],
+        
+    });
 
-function showValues() {
-    $('#alpha-value').html(chart.options.chart.options3d.alpha);
-    $('#beta-value').html(chart.options.chart.options3d.beta);
-    $('#depth-value').html(chart.options.chart.options3d.depth);
-}
+    function showValues() {
+        $('#alpha-value').html(chart.options.chart.options3d.alpha);
+        $('#beta-value').html(chart.options.chart.options3d.beta);
+        $('#depth-value').html(chart.options.chart.options3d.depth);
+    }
 
-// Activate the sliders
-$('#sliders input').on('input change', function () {
-    chart.options.chart.options3d[this.id] = parseFloat(this.value);
+    // Activate the sliders
+    $('#sliders input').on('input change', function () {
+        chart.options.chart.options3d[this.id] = parseFloat(this.value);
+        showValues();
+        chart.redraw(false);
+    });
+
     showValues();
-    chart.redraw(false);
-});
+};
 
-showValues();
+chartFunction(dates, illness, deaths, recovers, textTitle);
 
 
 //dynamic button
 const allRadio = document.querySelector("#allRadio");
 const all = document.querySelector("#all");
-
 all.textContent = "Общий прирост";
-chart.series.data  = illness;
 
 all.addEventListener('click', () => {
     if(allRadio.checked) {
         all.textContent = "Общий прирост";
-        chart.series.data  = illness;
-        console.log(chart.series.data );
+        textTitle = 'Распространение короновируса в России'
+        chartFunction(dates, illness, deaths, recovers, textTitle);
     } else {
         all.textContent = "Динамика прироста";
-        chart.series.data  = dynamicIllness;
-        console.log(chart.series.data );
+        textTitle = 'Динамика новых случаев'
+        chartFunction(dates, dynamicIllness, dynamicDeaths, dynamicRecovers, textTitle);
     }
 });
-
-
 
 
 export {illness, deaths, recovers};
