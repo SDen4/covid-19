feca1b3c7190750cd9da0d89b29915a8ea52d9b3
SDen4 <asw325@yandex.ru>
2020-04-28 23:45:21 +0500

    qr-code for mobile devices

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import "./scripts/parser";
 import "./scripts/curdate";
 import "./scripts/graph";
 import "./scripts/leftsection";
-import "./scripts/prognosis1"
-import "./scripts/prognosis2"
-import "./scripts/prognosis3"
+import "./scripts/prognosis1";
+import "./scripts/prognosis2";
+import "./scripts/prognosis3";
+import "./scripts/qrCode";
+
 import "./styles/css/main.css";
 import "./styles/scss/main.scss";
---
+++ src/scripts/qrCode.js
@@ -0,0 +1,18 @@
+const qrCode = document.querySelector('#qr-code'),
+    qrButton = document.querySelector('#qr-button');
+
+//open modal window
+qrButton.addEventListener('click', () => {
+    qrCode.style.display = 'flex';
+});
+
+//close modal window by click on window
+qrCode.addEventListener('click', () => {
+    qrCode.style.display = 'none';
+});
+
+//close modal window by esc abd enter buttons
+window.addEventListener('keydown', (e) => {
+    if(e.keyCode === 13 || e.keyCode === 27)
+    qrCode.style.display = 'none';
+});
