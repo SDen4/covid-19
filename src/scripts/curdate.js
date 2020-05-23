6df1e126d26ec655e60f2bb5f34e908140551c09
SDen4 <asw325@yandex.ru>
2020-05-23 14:24:52 +0500

    current time duration

--- src/scripts/curdate.js
+++ src/scripts/curdate.js
@@ -1,4 +1,9 @@
 const curDate = document.querySelector('#data__day');
 
-let now = new Date().toLocaleString("ru", {day: 'numeric', month: 'long', year: 'numeric', hour: '2-digit', minute: '2-digit'});
-curDate.textContent = `Сегодня: ${now}`;
+function time() {
+    let now = new Date().toLocaleString("ru", {day: 'numeric', month: 'long', year: 'numeric', hour: '2-digit', minute: '2-digit'});
+    curDate.textContent = `Сегодня: ${now}`;
+};
+time();
+
+setInterval(() => time(), 60000);
