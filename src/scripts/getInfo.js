0c029bd4ae27218c92c87c4dd3a1a26a4ee246b9
SDen4 <asw325@yandex.ru>
2020-10-19 23:09:55 +0500

    data is received

--- src/scripts/getInfo.js
+++ src/scripts/getInfo.js
@@ -1,8 +1,46 @@
-const test = document.querySelector('#all');
-const url = 'https://api.quarantine.country/api/v1/summary/region?region=florida&sub_areas=1';
+// const url = 'https://api.apify.com/v2/key-value-stores/1brJ0NLbQaJKPTWMO/records/LATEST?disableRedirect=true';
+// const url = 'https://api.quarantine.country/api/v1/spots/year?region=russia';
+// const url = 'https://api.apify.com/v2/datasets/5JO5GL1h8Qv1CnG0m/items?format=json';
+// const url = 'https://api.covid19api.com/country/russia/status/confirmed';
+// const url = 'https://api.covid19api.com/country/russia/status/recovered';
+// const url = 'https://api.covid19api.com/country/russia/status/deaths';
 
-test.addEventListener('click', async() => {
-    console.log('test button!')
-    let response = await fetch(url);
-    console.log(response);
-});
+const url = 'https://api.covid19api.com/country/russia/status/';
+
+const ill = document.querySelector('#data__display_ill');
+const dead = document.querySelector('#data__display_dead');
+const recover = document.querySelector('#data__display_recover');
+
+
+//temp get data grom api 
+ill.addEventListener('click', ()=>{
+    getData('confirmed');
+})
+dead.addEventListener('click', ()=>{
+    getData('deaths');
+})
+recover.addEventListener('click', ()=>{
+    getData('recovered');
+})
+
+
+async function getData(type) {
+    let response = await fetch(url+type);
+    if (response.ok) {
+        let json = await response.json();
+
+        console.log(json);
+
+        let arr = []; // new array of data
+
+        //create new arrays of data
+        json.map(res => {
+            let dayValue = res.Cases;
+            arr.push(dayValue);
+        })
+
+        console.log(arr);
+    } else {
+        console.log("Ошибка HTTP: " + response.status);
+    };
+};
