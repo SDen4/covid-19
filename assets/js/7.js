e574fa6501d8d7f3e9551ad8563f54272f2762ea
SDen4 <asw325@yandex.ru>
2020-10-24 14:35:01 +0500

    delete extra code

---
+++ assets/js/7.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{338:function(n,o,p){"use strict";p.r(o);for(var r=18328,s=[],a=0;a<82;a++)s.push("");s[82]=r;for(var w=0;w<17;w++)r=parseInt(1.16*r),s[83+w]=r;o.default=s}}]);
